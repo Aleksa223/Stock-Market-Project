6f377b1a68f9099b60c306a811e519bb08133d7d
Aleksa Pesic <124280965+Aleksa223@users.noreply.github.com>
2023-11-02 18:25:47 -0500

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,13 +1,13 @@
-import scraping  # This will import your scraping.py file as a module
+import scraping 
 import subprocess
 
-def run_exist():
+def run_charts():
     # Execute exist.py as a subprocess, as if it were run from the command line
-    subprocess.run(['python', 'exist.py'], check=True)
+    subprocess.run(['python', 'charts.py'], check=True)
 
 if __name__ == "__main__":
     # Run the main function from scraping.py
     scraping.main()
 
     # After scraping.py finishes, run exist.py
-    run_exist()
+    run_charts()
